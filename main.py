45437a978108de8ea2c1b8c76f0794eaae1287c8
phobyjun <phobyjun@gmail.com>
2022-10-14 13:31:28 +0900

    ADD: Internal error handling when detecting barcode

--- main.py
+++ main.py
@@ -17,11 +17,17 @@ default_s3_route = "https://naeng-bu-test.s3.ap-northeast-2.amazonaws.com/"
 
 @app.get("/barcode/{image_url}")
 async def barcode_detection_api(image_url: str):
-    image = image_from_url(default_s3_route + image_url)
+    try:
+        image = image_from_url(default_s3_route + image_url)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=e.__str__())
+
     try:
         barcode = detect(image)
     except NotFoundBarcodeException as error:
         raise HTTPException(status_code=404, detail=error.__str__())
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=e.__str__())
 
     try:
         expiration_date = parse(barcode)
@@ -31,6 +37,8 @@ async def barcode_detection_api(image_url: str):
         }
     except NotGS1BarException as error:
         raise HTTPException(status_code=404, detail=error.__str__())
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=e.__str__())
 
 
 handler = Mangum(app)
